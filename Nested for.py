55c26b689ef1a208283ecd2d7dcc4f438a1f1653
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-18 22:29:51 +0100

    script added

--- Nested for.py
+++ Nested for.py
@@ -2,3 +2,23 @@
 
 # 3, 2, 1
 # 3, 2, 1
+
+def sequence(low, high):
+    # Complete the outer loop range to make the loop run twice
+    # to create two rows
+    x = low
+    y = high
+    for x in range(2): 
+        # Complete the inner loop range to print the given variable
+        # numbers starting from "high" to "low" 
+        # Hint: To decrement a range parameter, use negative numbers
+        for y in range(3,0,-1): 
+            if y == low:
+                # Don’t print a comma after the last item
+                print(str(y)) 
+            else:
+                # Print a comma and a space between numbers
+                print(str(y), end=", ") 
+
+sequence(1, 3)
+# Should print the sequence 3, 2, 1 two times, as shown above.
