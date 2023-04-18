12bfddb33c2fa6321ba4429d52ad7fa567883c62
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-18 22:29:22 +0100

    nested for added

---
+++ Nested for.py
@@ -0,0 +1,4 @@
+# Fill in the blanks to complete the “sequence” function. This function should print a sequence of numbers in descending order, from the given "high" variable to the given "low" variable.  The range should make the loop run two times. Complete the range sequences in the nested loops so that the “sequence(1, 3)” function call prints the following:
+
+# 3, 2, 1
+# 3, 2, 1
