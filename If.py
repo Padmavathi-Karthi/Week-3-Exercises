6da7a0ee80216ba171d0c5379aeb82960f1ca1ae
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-18 22:27:23 +0100

    if condition added

---
+++ If.py
@@ -0,0 +1,2 @@
+# Fill in the blanks to complete the function “digits(n)” to count how many digits the given number has. For example: 25 has 2 digits and 144 has 3 digits. 
+# Tip: you can count the digits of a number by dividing it by 10 once per digit until there are no digits left.
