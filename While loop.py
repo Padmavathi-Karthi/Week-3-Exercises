b6c533b6b9dacbc9b0a12f6ebd1f0f798e0fd0fc
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-18 22:22:02 +0100

    another script added

--- While loop.py
+++ While loop.py
@@ -9,3 +9,12 @@ while result <= 50:
 print("Done")
 
 print(" ")
+
+# Fill in the blanks to print the numbers 1 through 7
+
+number = 1 # Initialize the variable
+while number<=7: # Complete the while loop condition
+    print(number, end=" ")
+    number +=1 # Increment the variable
+
+# Should print 1 2 3 4 5 6 7 
