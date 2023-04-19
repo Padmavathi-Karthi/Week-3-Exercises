9d2a32fe4f17ef5dd24d20f14198929eea5940bd
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-19 10:21:10 +0100

    changes added

--- For loop.py
+++ For loop.py
@@ -11,6 +11,8 @@ for number in range(5,-1,-1):
 # 1
 # 0
 
+print(" ")
+
 # Fill in the blanks to complete the “odd_numbers” function. This function should return a space-separated string of all odd positive numbers, up to and including the “maximum” variable that's passed into the function. Complete the for loop so that a function call like “odd_numbers(6)” will return the numbers “1 3 5”.
 
 def odd_numbers(maximum):
@@ -35,3 +37,5 @@ print(odd_numbers(10)) # Should be 1 3 5 7 9
 print(odd_numbers(1))  # Should be 1
 print(odd_numbers(3))  # Should be 1 3
 print(odd_numbers(0))  # No numbers displayed
+
+print(" ")
