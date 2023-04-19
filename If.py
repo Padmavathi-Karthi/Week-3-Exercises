ad9e1953f2ae19390f011652541640372592073f
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-19 10:22:29 +0100

    changes added

--- If.py
+++ If.py
@@ -18,6 +18,8 @@ print(digits(144))  # Should print 3
 print(digits(1000)) # Should print 4
 print(digits(0))    # Should print 1
 
+print(" ")
+
 # Fill in the blanks to complete the “counter” function. This function should count down from the “start” to “stop” variables when “start” is bigger than “stop”. Otherwise, it should count up from “start” to “stop”. Complete the code so that a function call like “counter(3, 1)” will return “Counting down: 3, 2, 1” and “counter(2, 5)” will return “Counting up: 2, 3, 4, 5”.
 
 def counter(start, stop):
@@ -43,3 +45,5 @@ def counter(start, stop):
 print(counter(1, 10)) # Should be "Counting up: 1,2,3,4,5,6,7,8,9,10"
 print(counter(2, 1)) # Should be "Counting down: 2,1"
 print(counter(5, 5)) # Should be "Counting up: 5"
+
+print(" ")
