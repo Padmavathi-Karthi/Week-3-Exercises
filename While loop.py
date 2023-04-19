5358559b70acfeca338ab64b305ada46f77b3cb0
Padmavathi-Karthi <padhukarthi17@gmail.com>
2023-04-19 10:19:03 +0100

    script added

--- While loop.py
+++ While loop.py
@@ -20,3 +20,18 @@ while number<=7: # Complete the while loop condition
 # Should print 1 2 3 4 5 6 7 
 
 print(" ")
+
+print(" ")
+
+
+# Exercise 3:
+
+x = 1
+sum = 0
+while x <= 10:
+    sum += x
+    x += 1
+print(sum)
+# Should print 55
+
+print(" ")
